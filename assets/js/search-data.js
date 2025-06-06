61e4766456c30229b4d81888aad82db5bf025c13
dev-onejun <wjjgjk@naver.com>
2025-06-06 01:42:44 +0000

    Deploying to gh-pages from @ dev-onejun/CV@b3fb20d5d00cebd4ed216cccd4612b94c4b6e68e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,20 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/CV/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/CV/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/CV/teaching/";
-          },
         },{id: "dropdown-publications",
               title: "publications",
               description: "",
